b6f71200818444243570643d68d62ca64b92038e
tushar-c23 <tusharc.cse@gmail.com>
2023-04-24 12:25:17 +0530

    MongoDB connection fixed

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,28 +1,26 @@
 const { MongoClient } = require("mongodb");
 const Db = process.env.ATLAS_URI;
 const client = new MongoClient(Db, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
-
-let _db;
-
+ 
+var _db;
+ 
 module.exports = {
-    connectToServer: function (callback) {
-        console.log("Into the function")
-        client.connect(Db, function (err, db) {
-            // Verify we got a good "db" object
-            console.log(Db);
-            if (db)
-            {
-                _db = db.db("employees");
-                console.log("Successfully connected to MongoDB.");
-            }
-            return callback(err);
-        });
-    },
-
-    getDb: function () {
-        return _db;
-    },
+  connectToServer: function (callback) {
+    client.connect(function (err, db) {
+      // Verify we got a good "db" object
+      if (db)
+      {
+        _db = db.db("employees");
+        console.log("Successfully connected to MongoDB."); 
+      }
+      return callback(err);
+         });
+  },
+ 
+  getDb: function () {
+    return _db;
+  },
 };
--- server/server.js
+++ server/server.js
@@ -1,19 +1,43 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
-
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
-
+// get driver connection
 const dbo = require("./db/conn");
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.ATLAS_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+ 
 app.listen(port, () => {
-    dbo.connectToServer(function (err) {
-        if(err) console.error(err);
-    });
-    console.log(`Server is running on port: ${port}`);
+  // perform a database connection when server starts
+  console.log(`Server is running on port: ${port}`);
 });
